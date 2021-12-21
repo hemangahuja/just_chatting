040a23d795d3719a53ea0b6316dd437082663a71
hemang <hemangahujaeotw@gmail.com>
2021-12-22 02:07:36 +0530

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Pusher = require("pusher");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.APP_KEY,
+  secret: process.env.APP_SECRET,
+  cluster: process.env.APP_CLUSTER,
+  useTLS: true,
+});
+
+export default async function handler(req, res) {
+  const { message, sender } = req.body;
+  await pusher.trigger("my-channel", "my-event", {
+    message,
+    sender,
+  });
+
+  res.json({ message: "completed" });
 }
