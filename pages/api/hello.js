bdaa44f60ee597772e21ceb6f10e2db67fb822a6
hemangahuja <76509238+hemangahuja@users.noreply.github.com>
2021-12-23 20:54:49 +0530

    env variable fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ const Pusher = require("pusher");
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
-  key: process.env.MIX_PUSHER_APP_KEY,
+  key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
   cluster: process.env.PUSHER_APP_CLUSTER,
   useTLS: true,
